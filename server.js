bfb419645c8e80e7bc4e81685d254606673baf5c
anikettt21 <gadeaniket37@gmail.com>
2025-02-25 10:31:15 +0530

    Update server.js

--- server.js
+++ server.js
@@ -169,3 +169,6 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+app.use(cors({
+  origin: "https://anikettt21.github.io"
+}));
